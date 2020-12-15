ee37f06ddc96b7a858c6f2d99090bd57bc623fcb
Taisuke Fukuno <fukuno@jig.jp>
2020-12-16 07:59:37 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# zen-wasm-fib
+ Zenでフィボナッチ
---
+++ zen-cache/o/_cVNmSDYiBxPeYi0nH0KuLwUti2Q0jRDbB-EmvSu14AVBiSpS_OSuwItQbhxJdzm/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int32_t fib(int32_t n);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+
+#endif // MAIN_H
